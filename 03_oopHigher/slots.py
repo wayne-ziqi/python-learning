6d567ce59719368baf54a1b88c43a66986956f96
wayne-ziqi <972282146@qq.com>
2022-02-20 16:00:32 +0800

    children are beyond slots

--- 03_oopHigher/slots.py
+++ 03_oopHigher/slots.py
@@ -6,6 +6,10 @@
 # @Software : PyCharm
 # @Email    : wayne-ziqi@gmail.com
 
+'探究类的slot功能'
+
+####################
+print("\ntest1\n")
 class Student(object):
     pass
 
@@ -21,3 +25,28 @@ from types import MethodType
 s.set_age = MethodType(set_age, s)
 s.set_age(25)
 print(s._age)
+
+##############################
+print("\ntest2\n")
+class StudentSlot(object):
+    __slots__ = ('_name', '_age')
+
+ss = StudentSlot()
+ss._name = 'Michael'
+ss._age = 25
+
+class GraduateStudent(StudentSlot):
+    pass
+
+#slots不能限制子类
+g = GraduateStudent()
+g._score = 77
+print("g's score is {0}".format(g._score))
+
+def set_score(self, score):
+    self._score = score
+
+GraduateStudent.set_score = set_score #相当于存储了函数指针，不止给对象绑定方法，而写给类绑定方法
+
+g.set_score(88)
+print("g's score is {0}".format(g._score))
