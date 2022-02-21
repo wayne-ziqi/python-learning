6540968ede1c673af2452cfbd590f8534666bdc2
wayne-ziqi <972282146@qq.com>
2022-02-21 22:58:14 +0800

    subProc.py

--- 05_process/subProc.py
+++ 05_process/subProc.py
@@ -13,3 +13,5 @@ import subprocess
 print("%s$ nslookup www.python.org" % __author__)
 r = subprocess.call(["nslookup", "www.python.org"])
 print("exit code :", r)
+
+#父进程所有Python对象都必须通过pickle序列化再传到子进程去，所以，如果multiprocessing在Windows下调用失败了，要先考虑是不是pickle失败了
